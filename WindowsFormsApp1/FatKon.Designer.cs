97d08e03e838c573e003aaa48d320de1a0789519
xleatz <chatlaks_1992@hotmail.com>
2017-11-16 13:03:56 +0300

    Add files via upload

---
+++ WindowsFormsApp1/FatKon.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class FatKon
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(543, 183);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::FatKon.Properties.Resources.Action_db_add_icon__1_;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(470, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 72);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Güncelle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(86, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 215);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "TARİH:";
+            // 
+            // FatKon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(543, 261);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FatKon";
+            this.Text = "Fatura Kontrol ";
+            this.Load += new System.EventHandler(this.FatKon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/FatKon.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApp1
+{
+    public partial class FatKon : Form
+    {
+        public FatKon()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection bag = new OleDbConnection("Provider=Microsoft.Ace.Oledb.12.0;Data Source=FatKon.accdb");
+        OleDbDataAdapter da;
+        System.Data.DataTable tablo = new System.Data.DataTable();
+        OleDbCommandBuilder cb;
+        TimeSpan fark;
+        double gunfark;
+
+        void Listele()
+        {
+            tablo.Clear();  
+            da = new OleDbDataAdapter("SELECT * From alaybey ", bag);
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+        }
+        void renklendir()
+        {
+
+            for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+            {
+
+                fark = Convert.ToDateTime(dataGridView1.Rows[i].Cells["sontarih"].Value.ToString()) - Convert.ToDateTime(DateTime.Now.ToShortDateString());
+                gunfark = fark.TotalDays;
+                bool odeme = Convert.ToBoolean(dataGridView1.Rows[i].Cells["odendi"].Value);
+                if (gunfark <= 3 && odeme == false)
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Red;
+                }
+                else if (gunfark > 3 && gunfark < 7 && odeme == false)
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Yellow;
+                }
+                else
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.White;
+                }
+            }
+        }
+        private void FatKon_Load(object sender, EventArgs e)
+        {
+ label1.Text = DateTime.Now.ToShortDateString();
+            Listele();
+            dataGridView1.Columns[0].Visible = false;
+            renklendir();
+            dataGridView1.Columns[1].HeaderText = "Fatura Cinsi";
+            dataGridView1.Columns[2].HeaderText = "Fatura Tarihi";
+            dataGridView1.Columns[3].HeaderText = "Son Ödeme Tarihi";
+            dataGridView1.Columns[4].HeaderText = "Tutar";
+            dataGridView1.Columns[5].HeaderText = "Ödeme Yapıldı";
+ 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                try
+                {
+                    cb = new OleDbCommandBuilder(da);
+                    da.Update(tablo);
+                    MessageBox.Show("Kayıt güncellendi");
+                    renklendir();
+                }
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         
+                Excel.Application excel = new Excel.Application();
+                excel.Visible = true;
+                object Missing = Type.Missing;
+                Workbook workbook = excel.Workbooks.Add(Missing);
+                Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+                int StartCol = 1;
+                int StartRow = 1;
+                for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                {
+                    Range myRange = (Range)sheet1.Cells[StartRow, StartCol + j];
+                    myRange.Value2 = dataGridView1.Columns[j].HeaderText;
+                }
+                StartRow++;
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    for (int j = 0; j < dataGridView1.Columns.Count; j++)
+                    {
+
+                        Range myRange = (Range)sheet1.Cells[StartRow + i, StartCol + j];
+                        myRange.Value2 = dataGridView1[j, i].Value == null ? "" : dataGridView1[j, i].Value;
+                        myRange.Select();
+
+
+                    }
+                }
+            }
+        }
+    }
+    
+
